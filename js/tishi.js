4cffa563a61bde8282f84517897687082dc51a1a
大脸猫 <32319697+aileheart@users.noreply.github.com>
2019-01-06 20:47:36 +0800

    Add files via upload

---
+++ js/tishi.js
@@ -0,0 +1,41 @@
+// JavaScript Document
+
+function tishi()
+{
+alert("关闭对话框后开始添加规则，更多好玩请关注公众号“APP应用分享螃蟹君”");
+}
+
+function xunlei()
+{
+alert("迅雷证书被封，已经不能安装了。不便之处，敬请谅解！更多好玩关注微信公众号“APP应用分享螃蟹君”。");
+}
+
+function katong()
+{
+alert("因为素材的原因，第二弹卡通微信主题的主题替换包只支持最高6.5.7版本的微信。不便之处，敬请谅解。");
+}
+
+function qqcb()
+{
+alert("该qq主题只支持qq版本7.x一下的qq使用！");
+}
+
+function wxsq()
+{
+alert("【激活码输入：随便输入】，更多好玩，关注公众号：APP应用分享螃蟹君");
+}
+
+function ipabao()
+{
+alert("此弹微信主题，不提供ipa直装包。可以根据替换教程自行替换安装。");
+}
+
+function ph()
+{
+alert("因该应用开始收费，已经下架，谢谢您的支持！更多好玩关注公众号：APP应用分享螃蟹君");
+}
+
+function xd()
+{
+alert("因为某些原因店铺暂停营业，如需购买主题可以使用 iMessage 或邮件联系扎比，aileheart@foxmail.com。谢谢支持。");
+}
